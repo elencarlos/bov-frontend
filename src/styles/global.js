85f2a808e15a120f3f73b4e240855e56c29ae12b
Elencarlos Soares <elencarlos@gmail.com>
2022-06-02 17:05:39 -0300

    Initial commit

---
+++ src/styles/global.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+
+export default createGlobalStyle`
+  ${reset}
+
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500&display=swap');
+
+  html {
+    --white: #ffffff;
+  }
+`
