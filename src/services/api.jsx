4f782eaed73fcda54fa4030ff903a1b517665467
Elencarlos Soares <elencarlos@uft.edu.br>
2022-06-04 22:55:47 -0300

    netlify

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const url = '/api'
+const apiUrl = import.meta.env.VITE_API_URL
+const url = apiUrl ? ${apiUrl}+'/api' : '/api'
 
 const http = axios.create({
   baseURL: url,
