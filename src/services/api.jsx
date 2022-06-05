8c9561169111533e4f93f9c7d22a53b8290fdd3c
Elencarlos Soares <elencarlos@uft.edu.br>
2022-06-04 22:59:10 -0300

    fix api url

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiUrl = import.meta.env.VITE_API_URL
-const url = apiUrl ? ${apiUrl}+'/api' : '/api'
+const url = apiUrl ? `${apiUrl}/api` : '/api'
 
 const http = axios.create({
   baseURL: url,
